f9cbc733d408cc56e87dcb04cbf0487a00a1cc47
Shane Mc Cormack <shane.mccormack@melbourne.co.uk>
2013-10-26 17:08:31 +0100

    Holding page.

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Nothing to see here...</title>
+</head>
+<body>
+<div class="content">
+<h1>Hello World!</h1>
+<p>Unfortunately, there is nothing to see here...</p>
+</div>
+</body>
+</html>
